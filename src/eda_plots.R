835fbb19ed4c73bc11702f803080a31249c54b58
evanythn <evan.ythn@gmail.com>
2018-11-22 15:20:24 -0800

    finalized which plots to use, now creating publication quality images

--- src/eda_plots.R
+++ src/eda_plots.R
@@ -18,13 +18,15 @@ suppressMessages(library(tidyverse))
 
 main <- function(){
       
-      tidy_day_df <- read_csv("data/tidy_day.csv")
+      tidy_df <- read_csv("data/tidy_day.csv")
       
-      tidy_day_df <- tidy_day_df %>% 
+      tidy_df <- tidy_df %>% 
             filter(rental_type == "both")
       
+      # tidy_df = read_csv(input_file)
+      
       #reorder factors in data so that it plots in logical order
-      tidy_day_df <- tidy_day_df %>% 
+      tidy_df <- tidy_df %>% 
             mutate(rentalship = fct_relevel(rentalship, c("Low","Mid","High")),
                    weathersit = as.factor(weathersit),
                    weathersit = fct_recode(weathersit, 
@@ -33,43 +35,86 @@ main <- function(){
                                            light_rain = "3"))
       
       # show the ridership histogram and display the lines where the cutoffs were made
-      ranges <- unname(quantile(tidy_day_df$rental_num,c(0.33,0.66)))
+      ranges <- unname(quantile(tidy_df$rental_num,c(0.33,0.66)))
       
-      tidy_day_df %>% 
+      ridership_histo <- tidy_df %>% 
             ggplot(aes(x = rental_num)) +
             geom_histogram(colour = "black", bins = 30, fill = "dodgerblue3") +
-            geom_vline(xintercept = ranges, size = 1.5, lty = 2)
+            geom_vline(xintercept = ranges, size = 1.5, lty = 2) +
+            labs(x = "Daily Rental Numbers",
+                 title = "Histogram of Daily Bike Rentals with Quantile Lines") +
+            annotate("text",
+                     x = 1250,
+                     y = 45,
+                     label = paste("Q33 = ",round(ranges[1],0),"\n Q66 = ",round(ranges[2],0))) +
+            annotate("text",
+                     x = 2500,
+                     y = 10,
+                     label = "Low",
+                     colour = "white",
+                     size = 7) +
+            annotate("text",
+                     x = 4500,
+                     y = 10,
+                     label = "Mid",
+                     colour = "white",
+                     size = 7) +
+            annotate("text",
+                     x = 6500,
+                     y = 10,
+                     label = "High",
+                     colour = "white",
+                     size = 7) +
+            theme(text = element_text(size = 15))
+      
       
       # useful to see ridership during weather conditions
-      tidy_day_df %>% 
+      weather_rentalship_heatmap <- tidy_df %>% 
       ggplot(aes(x = rentalship, y = weathersit)) +
-            geom_bin2d() 
-      
-      # Another way to see ridership during weather conditions
-      tidy_day_df %>% 
-            ggplot(aes(x = as.factor(weathersit), y = rental_num)) +
-            geom_boxplot() +
-            geom_jitter()
+            geom_bin2d() +
+            labs(x = "Rentalship Categories",
+                 y = "Weather Situation",
+                 title = "Heatmap of Ridership for Different Weather Conditions") +
+            theme(text = element_text(size = 15))
+   
       
       # calculate actual temperature
-      tidy_day_df <- tidy_day_df %>% 
-            mutate(actual_temp = temp*(39+8) - 8)
-      
-      # useful to see ridership with reponse to temperature
-      # output suggests that converting temp back to original values not necessary
-      ggplot(tidy_day_df, aes(x = temp, y = rental_num)) +
-            geom_point()
+      tidy_df <- tidy_df %>% 
+            mutate(actual_atemp = temp*(50+16) - 16)
       
       #check to see if actual temp effects the ridership
-      ggplot(tidy_day_df, aes(x = actual_temp, y = rental_num)) +
-            geom_point()
+      ggplot(tidy_df, aes(x = actual_atemp, y = rental_num)) +
+            geom_point() +
+            labs(x = "'Feels Like' Temperature (Celcius)",
+                 y = "Daily Rental Numbers") +
+            theme(text = element_text(size = 15))
       
       # plot to see rentalship numbers by season
-      tidy_day_df %>% 
+      tidy_df %>% 
             ggplot(aes(x = as.factor(season), y = rental_num)) +
-                  geom_boxplot()
+                  geom_violin() +
+                  geom_jitter()
       
-
+      tidy_df %>% 
+            ggplot(aes(x = dteday, y = rental_num, colour = as.factor(season))) +
+            geom_line()
+      
+      tidy_df %>% 
+            ggplot(aes(x = year, y = rental_num)) +
+            geom_jitter()
+      
+      tidy_df %>% 
+            ggplot(aes(x = as.factor(year),y = rentalship)) +
+            geom_bin2d()
+      
+      #saving the plots to the location specified
+      # ggsave(paste(output_path,"_ridership_histo.png"), 
+      #        plot = ridership_histo, device = "png")
+      # ggsave(paste(output_path,"_weather_rentalship_heatmap.png"), 
+      #        plot = weather_rentalship_heatmap, device = "png")
+      
+      
+      
 }
 
 main()
---
+++ src/important_features.R
@@ -0,0 +1,15 @@
+# important_features.R
+#
+# This script takes the output from scikit learn's featrures_importance and plots them
+# and outputs a figure
+#
+#
+# Usage: Rscript src/important_features.R 
+
+suppressMessages(library(tidyverse))
+
+main <- function(){
+      
+      
+      
+}
